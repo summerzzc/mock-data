76a52f1add9a4ee5366eba6c621bbb8696cec479
zhangzc <zhangzc@joyowo.com>
2022-09-22 10:46:18 +0800

    remove node_modules

--- config.ts
+++
@@ -1,6 +0,0 @@
-import crmTemplate from './template/crm';
-export const config = {
-  port: 8080,
-  https: false,
-  template: crmTemplate,
-};
--- types/global/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare global {
-  var mockData: any;
-}
-export {};
